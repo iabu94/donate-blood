4aa156b9a9321ab9b222e96c295c03c676386e98
Abdullah Mansoor <mansoorabdullah001@gmail.com>
2023-07-24 16:14:08 +0530

    Added Angular Materials

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from "@angular/fire/compat";
+import { FormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    BrowserAnimationsModule,
+
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    apiKey: 'AIzaSyCIIGHB-AxMdRRbJbephm2_2b3tEbgwg8A',
+    authDomain: 'findbloodlk.firebaseapp.com',
+    projectId: 'findbloodlk',
+    storageBucket: 'findbloodlk.appspot.com',
+    messagingSenderId: '701734775156',
+    appId: '1:701734775156:web:3897aeccbdf2a3dcb8a421',
+    measurementId: 'G-M257YX7S1S',
+  },
+  production: false,
+};
